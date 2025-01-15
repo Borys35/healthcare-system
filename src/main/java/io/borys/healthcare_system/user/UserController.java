aefe06361d9bc12aadf7920d39ffcd18ca901cb6
Borys35 <boryskac10@gmail.com>
2025-01-15 19:16:28 +0100

    Add [GET] /doctors/specializations to retrieve all available specializations

--- src/main/java/io/borys/healthcare_system/user/UserController.java
+++ src/main/java/io/borys/healthcare_system/user/UserController.java
@@ -2,11 +2,13 @@ package io.borys.healthcare_system.user;
 
 import io.borys.healthcare_system.global.DoctorPatientHelper;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/users")
@@ -14,10 +16,12 @@ public class UserController {
 
     private final UserRepository userRepository;
     private final DoctorPatientHelper doctorPatientHelper;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository, DoctorPatientHelper doctorPatientHelper) {
+    public UserController(UserRepository userRepository, DoctorPatientHelper doctorPatientHelper, UserService userService) {
         this.userRepository = userRepository;
         this.doctorPatientHelper = doctorPatientHelper;
+        this.userService = userService;
     }
 
     @GetMapping("")
@@ -52,9 +56,14 @@ public class UserController {
     @GetMapping("/doctors")
     Page<User> findAllDoctors(@RequestParam(required = false) DoctorSpecialization specialization, Pageable pageable) {
         if (specialization != null) {
-            return userRepository.findAllDoctorsByDoctorSpecialization(specialization, pageable);
+            return userService.findAllDoctorsByDoctorSpecialization(specialization, pageable);
         } else {
-            return userRepository.findAllDoctors(Pageable.unpaged());
+            return userService.findAllDoctors(Pageable.unpaged());
         }
+    }
+
+    @GetMapping("/doctors/specializations")
+    Page<String> getAllDoctorSpecializations(Pageable pageable) {
+        return userService.getAllDoctorSpecializations(pageable);
     }
 }
--- src/main/java/io/borys/healthcare_system/user/UserService.java
+++ src/main/java/io/borys/healthcare_system/user/UserService.java
@@ -1,10 +1,15 @@
 package io.borys.healthcare_system.user;
 
+import io.jsonwebtoken.lang.Collections;
+import org.springframework.beans.support.PagedListHolder;
+import org.springframework.beans.support.SortDefinition;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class UserService {
@@ -18,7 +23,20 @@ public class UserService {
         return userRepository.findAllDoctors(pageable);
     }
 
-    Page<User> findAllDoctorsByDoctorSpecialization(Pageable pageable, DoctorSpecialization specialization) {
+    Page<User> findAllDoctorsByDoctorSpecialization(DoctorSpecialization specialization, Pageable pageable) {
         return userRepository.findAllDoctorsByDoctorSpecialization(specialization, pageable);
+    }
+
+    Page<String> getAllDoctorSpecializations(Pageable pageable) {
+        List<String> specializations = new java.util.ArrayList<>(Stream
+                .of(DoctorSpecialization.values())
+                .map(DoctorSpecialization::name)
+                .toList());
+        specializations.removeAll(Collections.of("UNSET", "PATIENT"));
+
+        int start = (int) pageable.getOffset();
+        int end = Math.min(start + pageable.getPageSize(), specializations.size());
+        List<String> pageContent = specializations.subList(start, end);
+        return new PageImpl<>(pageContent, pageable, specializations.size());
     }
 }
